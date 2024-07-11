4d48909cb88a491955d46ff0dfad231baa191a4f
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-11 23:13:10 +0530

    java

---
+++ src/Gui.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Gui {
+    
+    public static void main(String[] args) {
+        
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+ name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You age is "+ age);
+
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You age is "+ age);
+    }
+}
