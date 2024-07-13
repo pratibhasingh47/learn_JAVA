b968eccfbc4caf1706a4fa0e20ed81356ebe3384
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-13 11:21:13 +0530

    java

--- src/App.java
+++ src/App.java
@@ -115,35 +115,35 @@ public class App {
 
         
         // //SWITCH
-        // String day = "Friday";
+        String day = "Friday";
 
-        // switch (day) {
-        //     case "Sunday":
-        //         System.out.println("Today is Sunday");
-        //         break;
-        //     case "Monday":
-        //         System.out.println("Today is Monday");
-        //         break;
-        //     case "Tuesday":
-        //         System.out.println("Today is Tuesday");
-        //         break;
-        //     case "Wednesday":
-        //         System.out.println("Today is Wednesday");
-        //         break;
-        //     case "Thursday":
-        //         System.out.println("Today is Thursday");
-        //         break;
-        //     case "Friday":
-        //         System.out.println("Today is Friday");
-        //         break;
-        //     case "Saturday":
-        //         System.out.println("Today is Saturday");
-        //         break;
+        switch (day) {
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+            case "Monday":
+                System.out.println("Today is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+                break;
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday");
+                break;
+            case "Saturday":
+                System.out.println("Today is Saturday");
+                break;
         
-        //     default:
-        //         System.out.println("Invalid day options");
-        //         break;
-        // }
+            default:
+                System.out.println("Invalid day options");
+                break;
+        }
 
 
         // //LOGICAL OPERATOR
