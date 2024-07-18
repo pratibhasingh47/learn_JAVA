955f1c89c5f91dbd08fe99c66d9d207f23000709
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-19 00:02:36 +0530

    oops

--- src/Gui.java
+++ src/Gui.java
@@ -42,20 +42,20 @@ public class Gui {
 
         // -----------------------------------------------------
 
-        // JLabel label = new JLabel();
-        // label.setText("How you doing ?");
-        // label.setIcon(image);
-        // label.setHorizontalTextPosition(JLabel.CENTER);
-        // label.setVerticalTextPosition(JLabel.TOP);
-        // label.setForeground(Color.BLUE);
-        // label.setFont(new Font("MV Boli",Font.BOLD,20));
-        // label.setIconTextGap(10);
-        // label.setBackground(Color.BLACK);
-        // label.setOpaque(true);
-        // label.setBorder(border);
-        // label.setVerticalAlignment(JLabel.CENTER);
-        // label.setHorizontalAlignment(JLabel.CENTER);
-        // label.setBounds(0, 0, 250, 250); 
+        JLabel label = new JLabel();
+        label.setText("How you doing ?");
+        label.setIcon(image);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(Color.BLUE);
+        label.setFont(new Font("MV Boli",Font.BOLD,20));
+        label.setIconTextGap(10);
+        label.setBackground(Color.BLACK);
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(0, 0, 250, 250); 
 
         // ---------------------------------------------------
 
