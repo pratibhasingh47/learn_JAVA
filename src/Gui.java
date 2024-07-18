31414d32a7c6a14fabad0c6f932916aedb65ec0a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-19 00:02:15 +0530

    oops

--- src/Gui.java
+++ src/Gui.java
@@ -15,14 +15,14 @@ public class Gui {
 
     public static void main(String[] args) {
 
-        // String name = JOptionPane.showInputDialog("Enter your name");
-        // JOptionPane.showMessageDialog(null, "Hello " + name);
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
 
-        // int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
-        // JOptionPane.showMessageDialog(null, "You age is " + age);
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null, "You age is " + age);
 
-        // double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
-        // JOptionPane.showMessageDialog(null, "You age is " + height);
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You age is " + height);
 
         // --------------------------------------------------------------
 
