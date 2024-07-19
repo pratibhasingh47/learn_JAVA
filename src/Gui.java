b0fdb212121b8f67fb76d93db951b0f3d6787686
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-20 00:01:36 +0530

    java

--- src/Gui.java
+++ src/Gui.java
@@ -15,7 +15,7 @@ public class Gui {
 
     public static void main(String[] args) {
 
-        /String name = JOptionPane.showInputDialog("Enter your name");
+        String name = JOptionPane.showInputDialog("Enter your name");
         JOptionPane.showMessageDialog(null, "Hello " + name);
 
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
